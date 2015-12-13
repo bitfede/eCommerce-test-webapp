98a4a265de6fc09616598478c531bdd045302660
Fede Defaveri <f.g.defaveri@gmail.com>
2015-12-14 00:08:35 +0100

    fixed the purchase button and fixed a JS bug

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   #support stripe payments thru charges
   resources :charges
+
+  #route for showing purchases
+  resources :purcahses, only:[:show]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
