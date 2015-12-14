747d7bc89ac2940a229933c583cf3e27848bad73
Fede Defaveri <f.g.defaveri@gmail.com>
2015-12-14 23:07:50 +0100

    added a UUID to purchase object for more security

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,5 @@
-class PurchasesController < ApplicationController
- 
+class PurchasesController < ApplicationController 
  def show
-   @purchase = Purchase.find(params[:id])
+   @purchase = Purchase.find_by_uuid(params[:id])
  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213221111) do
+ActiveRecord::Schema.define(version: 20151214220109) do
 
   create_table "purchases", force: :cascade do |t|
     t.string   "email"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151213221111) do
     t.integer  "product_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "uuid"
   end
 
 end
