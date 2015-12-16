9dffea2e9d418cd8da937efc4290423832b27b1d
Fede Defaveri <f.g.defaveri@gmail.com>
2015-12-16 14:43:48 +0100

    product type to seeds file for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+growth_hacking = Product.create(title: "Product Title", 
+	subtitle: "Product Subtitle", author: "Author Name", price: "4.99", sku:"PROD1" , description: %{<p>**Product description goes here**</p>
+            <p><strong>List of Product Features</strong></p>
+            <ul class="no-indent">
+                <li>product feature 1</li>
+                <li>product feature 2</li>
+                <li>product feature 3</li>
+                <li>product feature 4</li>
+            </ul>})
