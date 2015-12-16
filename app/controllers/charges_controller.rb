22d45738de912b2c1921cbf1dff3f0a37e7723e7
Fede Defaveri <f.g.defaveri@gmail.com>
2015-12-16 16:49:52 +0100

    changing product price - not from field value

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -11,7 +11,7 @@ class ChargesController < ApplicationController
 
 		  charge = Stripe::Charge.create(
 		    :customer    => customer.id,
-		    :amount      => params[:amount],
+		    :amount      => amount: product.price_in_cents,
 		    :description => 'Product Description',
 		    :currency    => 'usd'
 		  )
