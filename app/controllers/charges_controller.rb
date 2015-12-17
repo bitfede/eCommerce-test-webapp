d35899f513e919b1832c482adf852191ec76cc2c
Fede Defaveri <f.g.defaveri@gmail.com>
2015-12-17 23:18:04 +0100

    refined and fixed dynamic product purchasing

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,7 +2,7 @@ class ChargesController < ApplicationController
 
 		def create
       
-      product = Product.find_by_sku("MONSUB1")
+      product = Product.find(params[:product_id])
 
 		  customer = Stripe::Customer.create(
 		    :email => params[:stripeEmail],
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,10 +16,9 @@ growth_hacking = Product.create(title: "Gold Monthly Subscription Plan",
 	 sku:"MONSUB1" , 
 	 author_image_name: "teacher-image.jpg", 
 	 description: %{This is our best monthly plan! We will charge you monthly for unlimited access to our videos/courses/products/documents/guides/etc.. plus gold member perks -- this is an example of a monthly subscription type of product}, 
-  author_description: %{<p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
-  <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
-  </p>
-  <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
+  author_description: %{<p>Hey, I'm AUTHOR NAME, And I did this this this this and that and also this this this and that</p>
+  <p>More info about the author</p>
+  <p>Follow me on Twitter <a href="#" target="_blank">@AuthorTwitterName</a></p>})
 
 second_growth_hacking = Product.create(title: "Silver Monthly Subscription Plan", 
   id: "2",
@@ -29,10 +28,9 @@ second_growth_hacking = Product.create(title: "Silver Monthly Subscription Plan"
    sku:"MONSUB2" , 
    author_image_name: "teacher-image.jpg", 
    description: %{This is our most purchased monthly plan! We will charge you monthly for unlimited access to our videos/courses/products/documents/guides/etc.. plus silver member perks -- this is an example of a monthly subscription type of product}, 
-  author_description: %{<p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
-  <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
-  </p>
-  <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
+  author_description: %{<p>Hey, I'm AUTHOR NAME, And I did this this this this and that and also this this this and that</p>
+  <p>More info about the author</p>
+  <p>Follow me on Twitter <a href="#" target="_blank">@AuthorTwitterName</a></p>})
 
 third_growth_hacking = Product.create(title: "Bronze Monthly Subscription Plan", 
   id: "3",
@@ -42,8 +40,7 @@ third_growth_hacking = Product.create(title: "Bronze Monthly Subscription Plan",
    sku:"MONSUB3" , 
    author_image_name: "teacher-image.jpg", 
    description: %{This is our third affordable monthly plan! We will charge you monthly for unlimited access to our videos/courses/products/documents/guides/etc.. plus bronze member perks -- this is an example of a monthly subscription type of product}, 
-  author_description: %{<p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
-  <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
-  </p>
-  <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
+  author_description: %{<p>Hey, I'm AUTHOR NAME, And I did this this this this and that and also this this this and that</p>
+  <p>More info about the author</p>
+  <p>Follow me on Twitter <a href="#" target="_blank">@AuthorTwitterName</a></p>})
 
